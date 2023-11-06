a697fca405e6cd5061f30352351f7bd8b4c46966
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-11-06 15:51:59 +0530

    created react app & api for retrieving all course & course detail

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -1,15 +1,30 @@
-exports.getAllCourse=async(req,res)=>{
-    try{
+const courseData = require('../data/courses')
 
-    }catch(err){
+exports.getAllCourse = (req, res) => {
+    try {
+        res.status(200).json({ success: true, courseData })
+
+    } catch (err) {
         console.log(err)
+        res.status(500).json({ success: false, message: err })
+
     }
 }
 
-exports.getCourseDetail=async(req,res)=>{
-    try{
+exports.getCourseDetail = async (req, res) => {
+    try {
+        
+        const id = req.params.id
+        const course = courseData.find(course => course.id == id)
+        if (course) {
+            res.status(200).json({ success: true, course })
+        } else {
+            res.status(404).json({ success: false, error: 'Course not found' });
+        }
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
+        res.status(500).json({ success: false, message: err })
+
     }
 }
